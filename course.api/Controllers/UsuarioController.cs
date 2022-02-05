a1fb08d1e6e0e3419b55e77978381a9086ea2cee
Wendreo Fernandes <wendreo@outlook.com>
2022-02-05 15:40:03 -0300

    Added: 1st routes and setups

---
+++ course.api/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿using course.api.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace course.api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar(LoginViewModel loginViewModel)
+        {
+            return Ok(loginViewModel);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModel registroViewModel)
+        {
+            return Created("", registroViewModel);
+        }
+    }
+}
---
+++ course.api/Models/Usuarios/RegistroViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace course.api.Models.Usuarios
+{
+    public class RegistroViewModel
+    {
+        [Required(ErrorMessage = "O login é obrigatório")]
+        [StringLength(10)]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O E-mail é obrigatório")]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [StringLength(8)]
+        public string Senha { get; set; }
+    }
+}
